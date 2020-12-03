75717392e647cc2d21bdc9bc8dac56b9c981380e
yanbinbin <zl@zldeiMac-10.local>
2020-12-03 14:11:36 +0800

    2020-12-3 修改

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const server = http.createServer((req, res) => {
   const { url, method, headers } = req;
   const event = headers['x-github-event']
   const signature = headers['x-hub-signature']
-  let body; // 请求体
+  
   if (method === 'POST' && url === '/webhook') {
+    let body; // 请求体
     const buffer = [];
+    
     req.on('data', (fragment) => {
       buffer.push(fragment)
     })
-    
+
     req.on('end', (fragment) => {
       body = buffer.concat(fragment).toString()
       console.log('isSame key', sign(body) !== signature)
@@ -23,44 +25,45 @@ const server = http.createServer((req, res) => {
         res.end('Not Allowed')
         return
       }
-    })
-    // 通知 github 成功接收
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ ok: true }))
-    console.log(' 开启子进程 执行 .sh 脚本 -->  body', body)
-    // 开启子进程 执行 .sh 脚本 开始 cicd 
-    if (event === 'push') {
-      let payload = {}
-      try {
-        payload = JSON.parse(body);
-      } catch (err) {
-        return console.log('JSON.parse err', err)
-      }
-      console.log('sh path --> ', `./${payload.repository.name}.sh`)
-      const child = spawn('sh', [`./${payload.repository.name}.sh`])
 
-      // 输出日志
-      const buffer = []
-      child.stdio.on('data', (fragment) => {
-        buffer.push(fragment)
-      })
-      child.stdio.on('end', (fragment) => {
-        const logs = buffer.concat(fragment).toString()
-        console.log('logs -->', logs)
+      // 通知 github 成功接收
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ ok: true }))
+      console.log(' 开启子进程 执行 .sh 脚本 -->  body', body)
+      // 开启子进程 执行 .sh 脚本 开始 cicd 
+      if (event === 'push') {
+        let payload = {}
+        try {
+          payload = JSON.parse(body);
+        } catch (err) {
+          return console.log('JSON.parse err', err)
+        }
+        console.log('sh path --> ', `./${payload.repository.name}.sh`)
+        const child = spawn('sh', [`./${payload.repository.name}.sh`])
 
-        return
-        const { pusher, head_commit } = payload
-        const html = `
+        // 输出日志
+        const buffer = []
+        child.stdio.on('data', (fragment) => {
+          buffer.push(fragment)
+        })
+        child.stdio.on('end', (fragment) => {
+          const logs = buffer.concat(fragment).toString()
+          console.log('logs -->', logs)
+
+          return
+          const { pusher, head_commit } = payload
+          const html = `
         <div>前端 部署成功</div>
         <h3>部署日期: ${new Date}</h3>
         <h3>部署人  : xxx</h3>
         <h3>部署日志: ${logs.replace('\r\n', '<br />')}</h3>
         `
-        sendMail(html)
+          sendMail(html)
+        })
+      }
 
-      })
+    })
 
-    }
   } else {
     res.end('Not Found')
   }
