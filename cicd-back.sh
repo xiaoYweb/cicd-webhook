76dcc1069f0e22c321cb270b5b6137803b411624
yanbinbin <zl@zldeiMac-10.local>
2020-12-03 16:04:39 +0800

    新增build -f 操作 是否 覆盖之前的 镜像

--- cicd-back.sh
+++ cicd-back.sh
@@ -8,7 +8,7 @@ echo "拉取远程代码"
 git pull origin master
 echo "创建docke 镜像"
 #! 构建镜像 . 为当前文件夹 执行路径下的 Dockerfile 配置文件
-docker build -t cicd-back:1.0 .
+docker build -f ./Dockerfile -t cicd-back:1.0 .
 echo "终止 并 移除 之前的容器"
 docker stop cicd-back-container
 docker rm cicd-back-container
