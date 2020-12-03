6e6819de85591188f3f8d0b1defc1f12aad50209
yanbinbin <zl@zldeiMac-10.local>
2020-12-03 16:43:54 +0800

    2020-12-3 front sh

--- cicd-front.sh
+++ cicd-front.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WORK_PATH='/usr/projects/cicd-front'
 cd $WORK_PATH
-echo "清楚旧代码"
+echo "清除旧代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取远程代码"
@@ -10,11 +10,16 @@ echo "安装依赖"
 npm i
 echo "开始构建"
 npm run build
-echo "创建docke 镜像"
-#!  . 为当前所有文件
-docker build -t cicd-front:1.0 .
+
 echo "终止 并 移除 之前的容器"
 docker stop cicd-front-container
 docker rm cicd-front-container
+echo "移除之前的 docker 镜像"
+docker rmi cicd-front:1.0 -f
+echo "创建新 docker 镜像"
+#! 构建镜像 . 为当前文件夹 执行路径下的 Dockerfile 配置文件
+docker build -t cicd-front:1.0 .
+# docker build -f ./Dockerfile -t cicd-front:1.0 .
 echo "创建 并 启动 新容器"
-docker container run -port 80:80 --name cicd-front-container -d cicd-front:1.0
+# 错误命令 docker run -port 5000:5000 --name cicd-front-container -d cicd-front:1.0 
+docker run -d --name cicd-front-container -p 80:80 cicd-front:1.0
